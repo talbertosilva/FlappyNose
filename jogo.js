e15aa5d972219466332986dbdb489fef6e146edd
Tiago Silva <talbertosilva@ipvc.pt>
2022-03-16 14:10:47 +0000

    click to reset - update

--- jogo.js
+++ jogo.js
@@ -3,7 +3,7 @@ var tmap, smiley, plantsIndex, viewWalls = false;
 var x, y;
 let colected1 = false, colected2 = false, colected3 = false;
 
-let video, poseNet, prevnoseX, prevnoseY, xBadge, yBadge;
+let video, poseNet, prevnoseX, prevnoseY, xBadge, yBadge, xBird, yBird;
 let noseX = 50;
 let noseY = 220;
 let cursosESTG = 0;
@@ -16,10 +16,10 @@ function preload() {
 }
 
 function setup() {
-	createCanvas(640, 480);
+	createCanvas(620, 460);
 	smooth();
-  	noStroke();
-	
+	noStroke();
+
 	frameRate(30);
 	walls = createGraphics(640, 480);
 	initializeMap();
@@ -31,12 +31,12 @@ function setup() {
 
 	poseNet.on('pose', getPoses);
 
-	xBadge = 200;
-	xBadge2 = 392;
-	xBadge3 = 568;
+	xBadge = 190;
+	xBadge2 = 382;
+	xBadge3 = 558;
 	yBadge = random(208, 258);
-	yBadge2 = random(208, 258);
-	yBadge3 = random(208, 258);
+	yBadge2 = random(170, 220);
+	yBadge3 = random(188, 238);
 }
 
 
@@ -46,16 +46,17 @@ function getPoses(poses) {
 		let nX = poses[0].pose.keypoints[0].position.x;
 		let nY = poses[0].pose.keypoints[0].position.y;
 
-        noseY = lerp(noseY, nY, 0.5);
+		noseY = lerp(noseY, nY, 0.5);
 
-		if(cursosESTG<3){
+		if (cursosESTG < 3) {
 			noseX = noseX + 4;
 		} else {
 			noseX = 50;
+			noseY = 220;
 		}
 
 		smooth();
-		line (nX,nY,noseX,noseY);
+		line(nX, nY, noseX, noseY);
 	}
 }
 
@@ -63,52 +64,62 @@ function rectBall(rx, ry, rw, rh, bx, by, d) {
 
 	// first test the edges (this is necessary if the rectangle is larger
 	// than the ball) - do this with the Pythagorean theorem
-  
+
 	// if ball entire width position is between rect L/R sides
-	if (bx+d/2 >= rx-rw/2 && bx-d/2 <= rx+rw/2 && abs(ry-by) <= d/2) {
-	  return true;
+	if (bx + d / 2 >= rx - rw / 2 && bx - d / 2 <= rx + rw / 2 && abs(ry - by) <= d / 2) {
+		return true;
 	}
 	// if not, check if ball's entire height is between top/bottom of the rect
-	else if (by+d/2 >= ry-rh/2 && by-d/2 <= ry+rh/2 && abs(rx-bx) <= d/2) {
-	  return true;
+	else if (by + d / 2 >= ry - rh / 2 && by - d / 2 <= ry + rh / 2 && abs(rx - bx) <= d / 2) {
+		return true;
 	}
-  
+
 	// if that doesn't return a hit, find closest corner
 	// this is really just a point, so we can test if we've hit it 
 	// upper-left
-	var xDist = (rx-rw/2) - bx;  // same as ball/ball, but first value defines point, not center
-	var yDist = (ry-rh/2) - by;
-	var shortestDist = sqrt((xDist*xDist) + (yDist * yDist));
-  
+	var xDist = (rx - rw / 2) - bx;  // same as ball/ball, but first value defines point, not center
+	var yDist = (ry - rh / 2) - by;
+	var shortestDist = sqrt((xDist * xDist) + (yDist * yDist));
+
 	// upper-right
-	xDist = (rx+rw/2) - bx;
-	yDist = (ry-rh/2) - by;
-	var distanceUR = sqrt((xDist*xDist) + (yDist * yDist));
+	xDist = (rx + rw / 2) - bx;
+	yDist = (ry - rh / 2) - by;
+	var distanceUR = sqrt((xDist * xDist) + (yDist * yDist));
 	if (distanceUR < shortestDist) {  // if this new distance is shorter...
-	  shortestDist = distanceUR;      // ... update
+		shortestDist = distanceUR;      // ... update
 	}
-  
+
 	// lower-right
-	xDist = (rx+rw/2) - bx;
-	yDist = (ry+rh/2) - by;
-	var distanceLR = sqrt((xDist*xDist) + (yDist * yDist));
+	xDist = (rx + rw / 2) - bx;
+	yDist = (ry + rh / 2) - by;
+	var distanceLR = sqrt((xDist * xDist) + (yDist * yDist));
 	if (distanceLR < shortestDist) {
-	  shortestDist = distanceLR;
+		shortestDist = distanceLR;
 	}
-  
+
 	// lower-left
-	xDist = (rx-rw/2) - bx;
-	yDist = (ry+rh/2) - by;
-	var distanceLL = sqrt((xDist*xDist) + (yDist * yDist));
+	xDist = (rx - rw / 2) - bx;
+	yDist = (ry + rh / 2) - by;
+	var distanceLL = sqrt((xDist * xDist) + (yDist * yDist));
 	if (distanceLL < shortestDist) {
-	  shortestDist = distanceLL;
+		shortestDist = distanceLL;
 	}
-  
+
 	// test for collision
-	if (shortestDist < d/2) {  // if less than radius
-	  return true;             // return true
+	if (shortestDist < d / 2) {  // if less than radius
+		return true;             // return true
 	}
 	else {                     // otherwise, return false
+		return false;
+	}
+}
+
+function rectRect(x1, y1, w1, h1, x2, y2, w2, h2) {
+	// test for collision
+	if (x1+w1/2 >= x2-w2/2 && x1-w1/2 <= x2+w2/2 && y1+h1/2 >= y2-h2/2 && y1-h1/2 <= y2+h2/2) {
+	  return true;    // if a hit, return true
+	}
+	else {            // if not, return false
 	  return false;
 	}
   }
@@ -126,49 +137,64 @@ function draw() {
 
 
 	imageMode(CENTER);
-	if(colected1 == false) {
-		image(badge, xBadge, yBadge, 57, 40.6);
+	if (colected1 == false) {
+		image(badge, xBadge, yBadge, 28, 28);
 	}
-	if(colected2 == false) {
-		image(badge, xBadge2, yBadge2, 57, 40.6);
+	if (colected2 == false) {
+		image(badge, xBadge2, yBadge2, 32, 32);
 	}
-	if(colected3 == false) {
-		image(badge, xBadge3, yBadge3, 57, 40.6);
+	if (colected3 == false) {
+		image(badge, xBadge3, yBadge3, 36, 36);
 	}
-	
-	if(rectBall(xBadge, yBadge, 57, 40.6, noseX, noseY, 32) && colected1 !== true){
+
+	if (rectBall(xBadge, yBadge, 28, 28, noseX, noseY, 32, 32) && colected1 !== true) {
 		colected1 = true;
 		cursosESTG = cursosESTG + 1;
-	} else if(rectBall(xBadge2, yBadge2, 57, 40.6, noseX, noseY, 32) && colected2 !== true){
+	} else if (rectBall(xBadge2, yBadge2, 28, 28, noseX, noseY, 32, 32) && colected2 !== true) {
 		colected2 = true;
 		cursosESTG = cursosESTG + 1;
-	} else if(rectBall(xBadge3, yBadge3, 57, 40.6, noseX, noseY, 32) && colected3 !== true) {
+	} else if (rectBall(xBadge3, yBadge3, 28, 28, noseX, noseY, 32, 32) && colected3 !== true) {
 		colected3 = true;
 		cursosESTG = cursosESTG + 1;
 	}
 
 	/* -- TEXTO COM INFORMAÇÕES (coordenadas do nariz, cor detetada nessa coordenada, id da textura nessa coordenada) -- */
 
-	textSize(16);
+	textSize(48);
 	fill(255);
-	text(`Cursos: ${cursosESTG}`, 10, 10);
-	// text("Center: Map coordinates: " + round((noseX*40/640) * 100) / 100 + ", " + round((noseY*40/640) * 100) / 100, 10, 10);
-	// text("Over Color: " + color(walls.get(walls.width, walls.height)), 10, 30);
-	// text("Over Tile Index: " + tmap.getTileIndex(0, round(noseX*40/640), round(noseY*40/640)), 10, 50);
+	text(`${cursosESTG}`, 20, 40);
 	
 	walls.clear();
+	tmap.drawLayer(1, x*1.03, y*1.03, walls);
+
+	xBird = round(noseX * 40 / 640) + 1;
+	yBird = round(noseY * 40 / 640) + 1;
 
 	/* -- COLISÃO > caso esteja em cima dos objetos, o X/Y serão iguais aos X/Y antes da colisão --*/
-	if (tmap.getTileIndex(0, round(noseX*40/640), round(noseY*40/640)) !== 0) {
+	if (tmap.getTileIndex(0, round(xBird), round(yBird)) !== 0) {
 		noseX = 50;
 		noseY = prevnoseY;
 	} else {
 		imageMode(CENTER);
 		image(bird, noseX, noseY, 32, 32);
+
 		prevnoseY = noseY;
 	}
 }
-  
+
+function keyPressed() {
+	if (key == 'l' || key == 'L') viewWalls = !viewWalls;
+}
+
+function mouseClicked() {
+	if(cursosESTG === 3){
+		cursosESTG = 0;
+		colected1 = false;
+		colected2 = false;
+		colected3 = false;
+	}
+}
+
 /* -- INICIA O MAPA COM O DEVIDO TAMANHO EM TILES (40 x 30), 40 * 16 = 640 = tamanho da camera -- */
 function initializeMap() {
 	tmap.setPositionMode("MAP");
